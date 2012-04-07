516f17036e4579d6f403fa51156fdd17bea83d28
Felipe Kaufmann <felipekaufmann@gmail.com>
2012-04-07 12:19:30 +0200

    clean up and remove experimental multistage specs

--- spec/blazing/recipe_spec.rb
+++ spec/blazing/recipe_spec.rb
@@ -6,7 +6,7 @@ module Blazing
 
     describe '.init_by_name' do
       before :each do
-        class Blazing::Recipe::Dummy < Blazing::Recipe
+        class Recipe::Dummy < Blazing::Recipe
           def run(target_options = {})
             super target_options
           end
@@ -14,40 +14,26 @@ module Blazing
       end
 
       it 'initializes the correct recipe' do
-        Blazing::Recipe.init_by_name(:dummy).should be_a Blazing::Recipe::Dummy
+        Recipe.init_by_name(:dummy).should be_a Recipe::Dummy
       end
     end
 
     describe '.list' do
       it 'retunrs an array of the available recipe classes' do
-        Blazing::Recipe.list.should include Blazing::Recipe::Dummy
+        Recipe.list.should include Recipe::Dummy
       end
     end
 
     describe '#run' do
       it 'overrides global recipe options with target options' do
+        pending
         @production_url = '/some/target'
-        @dummy_recipe = Blazing::Recipe::Dummy.new(:some_option => 'global')
-        @config = Blazing::Config.new
+        @dummy_recipe = Recipe::Dummy.new(:some_option => 'global')
+        @config = Config.new
         @config.target(:production, @production_url, :some_option => 'target-specific')
         @config.instance_variable_set('@recipes', [@dummy_recipe])
         @dummy_recipe.run(:some_option => 'target-specific')
         @dummy_recipe.instance_variable_get('@options')[:some_option].should == 'target-specific'
-      end
-
-      context 'multistage recipes' do
-        let(:target) { double(:target, :options => {}).as_null_object }
-        let(:recipe) { Recipe.new }
-
-        it 'knows the state it is in' do
-
-        end
-
-        it 'runs the recipe action for the current stage' do
-          target.stage == :setup
-          recipe.should_receive(:setup)
-          recipe.run(target.options)
-        end
       end
     end
   end
